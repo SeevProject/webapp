bec6025f6f063b5cfdd299d219c208311d7eff97
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-18 15:35:02 +0300

    Mix both projects and add routing

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-	return (
-		<div className="min-h-screen bg-cvdark-1 text-cvlight-1">
-			<h1 className="text-lg font-bold">Hi, this is the frontend</h1>
-		</div>
-	);
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+import { Layout } from "./Layout";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<Layout />}>
+			<Route path="company" element={<h1>Hello</h1>} />
+		</Route>,
+	),
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router}></RouterProvider>
 	</React.StrictMode>,
 );
