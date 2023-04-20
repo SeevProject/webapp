e7773e7c70db27044e2e2b4195260d420fb1dad3
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-20 21:31:36 +0300

    Update theme colours in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,24 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {
-			colors: {
-				cvaccent: {
-					a: "#E1B462",
-					b: "#1C7FFC",
-				},
-				cvdark: {
-					1: "#232022",
-					2: "#302B2E",
-				},
-				cvgray: "#4a4a4a",
-				cvlight: {
-					1: "#FFFFFF",
-					2: "#A0A0A0",
-				},
-			},
+		colors: {
+			// accent colours
+			accentHighlight: "#E1B462",
+			accentFocus: "#1C7FFC",
+
+			// dark theme colours
+			bg: "#232022",
+			bgAlt: "#302B2E",
+			border: "#4a4a4a",
+			fgAlt: "#A0A0A0",
+			fg: "#FFFFFF",
+
+			// light theme colours
+			lightBg: "#FFFFFF",
+			lightBgAlt: "#EBEBEB",
+			lightBorder: "#C3C3C3",
+			lightFgAlt: "#676767",
+			lightFg: "#232022",
 		},
 	},
 	plugins: [require("prettier-plugin-tailwindcss")],
