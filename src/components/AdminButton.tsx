a307a002cbe73ff47ab84354402baa61ceeb90b3
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-29 09:53:29 +0300

    Fix errors by removing handleClick from props

--- src/components/AdminButton.tsx
+++ src/components/AdminButton.tsx
@@ -4,11 +4,9 @@ export function AdminButton(props: {
 	title?: string;
 	icon?: React.ReactNode;
 	style?: React.CSSProperties;
-	handleClick: () => void;
 }) {
 	return (
 		<button
-			onClick={props.handleClick}
 			className="flex flex-row items-center justify-center gap-2 rounded-md bg-accentPrimary px-2 py-1 font-bold transition-colors hover:bg-accentPrimary/70 focus:outline-none"
 			style={props.style}
 		>
--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -3,30 +3,30 @@ import { Button } from "../components/Button";
 import { AdminButton } from "../components/AdminButton";
 
 export function AdminNav() {
-  return (
-    <div>
-      <div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
-        <Link to={"/"}>
-          <p className="font-bold">Logo</p>
-        </Link>
-        <div className="flex flex-row ">
-          <AdminButton
-            style={{ borderRadius: "15px 0 0 15px" }}
-            title="Templates"
-          />
-          <AdminButton title="Companies" />
-          <AdminButton
-            style={{ borderRadius: "0 15px 15px 0" }}
-            title="Users"
-          />
-        </div>
+	return (
+		<div>
+			<div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
+				<Link to={"/"}>
+					<p className="font-bold">Logo</p>
+				</Link>
+				<div className="flex flex-row ">
+					<AdminButton
+						style={{ borderRadius: "15px 0 0 15px" }}
+						title="Templates"
+					/>
+					<AdminButton title="Companies" />
+					<AdminButton
+						style={{ borderRadius: "0 15px 15px 0" }}
+						title="Users"
+					/>
+				</div>
 
-        <div className="flex flex-row gap-2">
-          <Button title="Profile Icon" />
-        </div>
-      </div>
-    </div>
-  );
+				<div className="flex flex-row gap-2">
+					<Button handleClick={() => {}} title="Profile Icon" />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AdminNav;
