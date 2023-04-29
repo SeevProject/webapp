eb7634629a006d405c953f19861c8bc4fbf270a4
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-04-29 11:11:46 +0300

    fix JSX err, and delete one component

--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -1,32 +1,39 @@
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
-import { AdminButton } from "../components/AdminButton";
 
 export function AdminNav() {
-	return (
-		<div>
-			<div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
-				<Link to={"/"}>
-					<p className="font-bold">Logo</p>
-				</Link>
-				<div className="flex flex-row ">
-					<AdminButton
-						style={{ borderRadius: "15px 0 0 15px" }}
-						title="Templates"
-					/>
-					<AdminButton title="Companies" />
-					<AdminButton
-						style={{ borderRadius: "0 15px 15px 0" }}
-						title="Users"
-					/>
-				</div>
+  return (
+    <div>
+      <div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
+        <Link to={"/"}>
+          <p className="font-bold">Logo</p>
+        </Link>
+        <div className="flex flex-row ">
+          <Button
+            style={{ borderRadius: "15px 0 0 15px" }}
+            handleClick={() => {}}
+            title="Templates"
+          />
 
-				<div className="flex flex-row gap-2">
-					<Button handleClick={() => {}} title="Profile Icon" />
-				</div>
-			</div>
-		</div>
-	);
+          <Button
+            title="Companies"
+            style={{ borderRadius: "0" }}
+            handleClick={() => {}}
+          />
+
+          <Button
+            style={{ borderRadius: "0px 15px 15px 0px" }}
+            handleClick={() => {}}
+            title="Users"
+          />
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <Button handleClick={() => {}} title="Profile Icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminNav;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,19 @@
+import React from "react";
+
 export function Button(props: {
-	title?: string;
-	icon?: React.ReactNode;
-	handleClick: () => void;
+  title?: string;
+  icon?: React.ReactNode;
+  handleClick: () => void;
+  style?: React.CSSProperties;
 }) {
-	return (
-		<button
-			onClick={props.handleClick}
-			className="flex flex-row items-center justify-center gap-2 rounded-md bg-accentPrimary px-2 py-1 font-bold transition-colors hover:bg-accentPrimary/70 focus:outline-none"
-		>
-			{props.title && <p>{props.title}</p>}
-			{props.icon}
-		</button>
-	);
+  return (
+    <button
+      onClick={props.handleClick}
+      style={props.style}
+      className="flex flex-row items-center justify-center gap-2 rounded-md bg-accentPrimary px-2 py-1 font-bold transition-colors hover:bg-accentPrimary/70 focus:outline-none"
+    >
+      {props.title && <p>{props.title}</p>}
+      {props.icon}
+    </button>
+  );
 }
