22d1868b81da0ad2d2ba61e7c13e3f7d9330c5c8
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-10 19:44:07 +0300

    Fix text colour on buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 export function Button(props: {
-  title?: string;
-  icon?: React.ReactNode;
-  handleClick: () => void;
-  style?: React.CSSProperties; // you can add css styles with this props
-  className?: string; // you can add styles to button with tailwind
+	title?: string;
+	icon?: React.ReactNode;
+	handleClick: () => void;
+	style?: React.CSSProperties; // you can add css styles with this props
+	className?: string; // you can add styles to button with tailwind
 }) {
-  return (
-    <button
-      onClick={props.handleClick}
-      style={props.style}
-      className={`flex flex-row items-center justify-center gap-2 rounded-md bg-accentPrimary px-2 py-1 font-bold transition-colors hover:bg-accentPrimary/70 focus:outline-none ${props.className}`}
-    >
-      {props.title && <p>{props.title}</p>}
-      {props.icon}
-    </button>
-  );
+	return (
+		<button
+			onClick={props.handleClick}
+			style={props.style}
+			className={`flex flex-row items-center justify-center gap-2 rounded-md bg-accentPrimary px-2 py-1 text-background transition-colors hover:bg-accentPrimary/70 focus:outline-none ${props.className}`}
+		>
+			{props.title && <p>{props.title}</p>}
+			{props.icon}
+		</button>
+	);
 }
