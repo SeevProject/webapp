779083210583113325d702c9865d0da55540d8ff
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-05-25 16:44:06 +0300

    Remove css style  in admin button and add tailwind

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,103 +6,99 @@ import { Button } from "./Button";
 import { useState } from "react";
 
 export function Nav() {
-  const location = useLocation();
-  const [activeButton, setActiveButton] = useState("templates");
-  const navigate = useNavigate();
-  // try to logout
-  const logoutMutation = useMutation({
-    mutationKey: ["logout"],
-    mutationFn: tryLogout,
-  });
+	const location = useLocation();
+	const [activeButton, setActiveButton] = useState("templates");
+	const navigate = useNavigate();
+	// try to logout
+	const logoutMutation = useMutation({
+		mutationKey: ["logout"],
+		mutationFn: tryLogout,
+	});
 
-  // get user info from server
-  const userInfoQuery = useQuery({
-    queryKey: ["userInfo"],
-    queryFn: getUserInfo,
-    retry: 1,
-  });
+	// get user info from server
+	const userInfoQuery = useQuery({
+		queryKey: ["userInfo"],
+		queryFn: getUserInfo,
+		retry: 1,
+	});
 
-  return (
-    <div>
-      {location.pathname.startsWith("/admin") ? (
-        <div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
-          <Link to={"/"}>
-            <p className="font-bold">Logo</p>
-          </Link>
-          <div className=" flex flex-row rounded-md border border-border bg-box ">
-            <Button
-              style={{
-                backgroundColor:
-                  activeButton === "templates" ? "#555" : "#EBEBEB", // when user click this button the background color would change
-                color: activeButton === "templates" ? "#fff" : "#000000",
-                padding: "0 0.9rem",
-              }}
-              className="!important rounded-3xl hover:bg-accentPrimary"
-              handleClick={() => {
-                setActiveButton("templates");
-                navigate("/admin/templates");
-                {
-                  /* Go to these route if button is active */
-                }
-              }}
-              title="Templates"
-            />
+	return (
+		<div>
+			{location.pathname.startsWith("/admin") ? ( // Every route start with admin run this code
+				<div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
+					<Link to={"/"}>
+						<p className="font-bold">Logo</p>
+					</Link>
+					<div className=" flex flex-row rounded-full border border-border bg-box ">
+						<Button
+							className={`!important rounded-full px-4 py-1.5 hover:bg-accentPrimary hover:text-background ${
+								activeButton === "templates"
+									? "bg-accentPrimary text-background"
+									: "bg-box text-text"
+							}`}
+							handleClick={() => {
+								setActiveButton("templates");
+								navigate("/admin/templates");
+								{
+									/* Go to these route if button is active */
+								}
+							}}
+							title="Templates"
+						/>
 
-            <Button
-              title="Companies"
-              style={{
-                backgroundColor:
-                  activeButton === "companies" ? "#555" : "#EBEBEB",
-                color: activeButton === "companies" ? "#fff" : "#000000",
-                padding: "0 0.9rem",
-              }}
-              className="!important rounded-3xl hover:bg-accentPrimary"
-              handleClick={() => {
-                setActiveButton("companies");
-                navigate("/admin/companies");
-              }}
-            />
+						<Button
+							title="Companies"
+							className={`!important rounded-full  px-4 py-1.5 hover:bg-accentPrimary hover:text-background ${
+								activeButton === "companies"
+									? "bg-accentPrimary text-background"
+									: "bg-box text-text"
+							}`}
+							handleClick={() => {
+								setActiveButton("companies");
+								navigate("/admin/companies");
+							}}
+						/>
 
-            <Button
-              style={{
-                backgroundColor: activeButton === "users" ? "#555" : "#EBEBEB",
-                color: activeButton === "users" ? "#fff" : "#000000",
-                padding: "0 0.9rem",
-              }}
-              className="!important rounded-3xl hover:bg-accentPrimary"
-              handleClick={() => {
-                setActiveButton("users");
-                navigate("/admin/users");
-              }}
-              title="Users"
-            />
-          </div>
+						<Button
+							className={`!important rounded-full  px-4 py-1.5 hover:bg-accentPrimary hover:text-background ${
+								activeButton === "users"
+									? "bg-accentPrimary text-background"
+									: "bg-box text-text"
+							}`}
+							handleClick={() => {
+								setActiveButton("users");
+								navigate("/admin/users");
+							}}
+							title="Users"
+						/>
+					</div>
 
-          <div className="flex flex-row gap-2">
-            <Button handleClick={() => {}} title="Profile Icon" />
-          </div>
-        </div>
-      ) : (
-        <div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
-          <Link to={"/"}>
-            <p className="font-bold">Logo</p>
-          </Link>
-          <div className="flex flex-row gap-2">
-            {userInfoQuery.isLoading ? (
-              <p>Loading...</p>
-            ) : userInfoQuery.data ? (
-              <Button
-                title="Logout"
-                handleClick={() => logoutMutation.mutate()}
-              />
-            ) : (
-              <Link to={"/auth"}>
-                <Button title="Login" handleClick={() => {}} />
-              </Link>
-            )}
-          </div>
-        </div>
-      )}
-    </div>
-  );
+					<div className="flex flex-row gap-2">
+						<Button handleClick={() => {}} title="Profile Icon" />
+					</div>
+				</div>
+			) : (
+				// if it's not start with admin run this code
+				<div className="flex w-full flex-row justify-between gap-4 px-24 pt-8">
+					<Link to={"/"}>
+						<p className="font-bold">Logo</p>
+					</Link>
+					<div className="flex flex-row gap-2">
+						{userInfoQuery.isLoading ? (
+							<p>Loading...</p>
+						) : userInfoQuery.data ? (
+							<Button
+								title="Logout"
+								handleClick={() => logoutMutation.mutate()}
+							/>
+						) : (
+							<Link to={"/auth"}>
+								<Button title="Login" handleClick={() => {}} />
+							</Link>
+						)}
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
