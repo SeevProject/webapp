6407b7e6e4f42a9cd7cd50def36cf92126e0bd6d
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-27 22:42:15 +0300

    Add button to navigate to login page if not logged in

--- src/components/ProfileDropdown.tsx
+++ src/components/ProfileDropdown.tsx
@@ -5,8 +5,11 @@ import { tryLogout } from "../data/mutations";
 import { useMutation } from "@tanstack/react-query";
 import { Button } from "./Button";
 import { ProfileDropdownItem } from "./ProfileDropdownItem";
+import { useNavigate } from "react-router-dom";
 
 export default function ProfileDropdown(props: { notLoggedIn?: boolean }) {
+	const navigate = useNavigate();
+
 	// try to logout
 	const logoutMutation = useMutation({
 		mutationKey: ["logout"],
@@ -37,11 +40,19 @@ export default function ProfileDropdown(props: { notLoggedIn?: boolean }) {
 				leaveFrom="transform opacity-100 scale-100"
 				leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className="ring-black absolute origin-top-left rounded-lg border border-border bg-box ring-opacity-5 focus:outline-none ">
+				<Menu.Items className="ring-black absolute rounded-lg border border-border bg-box ring-opacity-5 focus:outline-none">
 					{/* if the user is not logged in */}
-					{!props.notLoggedIn ? (
+					{props.notLoggedIn ? (
 						<Menu.Item>
-							<p>You're not logged in</p>
+							<>
+								<p className="px-2 py-2 text-sm">You're not logged in</p>
+
+								<ProfileDropdownItem
+									text="Login"
+									icon={<HiCog className="text-gray-500 mr-2 inline h-5 w-5" />}
+									handleClick={() => navigate("/auth")}
+								/>
+							</>
 						</Menu.Item>
 					) : (
 						// if the user is logged in
