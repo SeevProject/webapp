d4ec01ac0acae76396b9a2a82123762320539ed0
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-06-30 16:07:15 +0300

    Fix err

--- src/pages/CompaniePage.tsx
+++ src/pages/CompaniePage.tsx
@@ -1,9 +1,27 @@
-import Popup from "../components/popup/Popup";
+import UserDataPopup from "../components/popup/UserDataPopup";
+import EditCompanyDataPopup from "../components/popup/EditCompanyDataPopup";
+import { useState } from "react";
 
 const CompaniePage = () => {
+	const [isPopupOpen, setPopupOpen] = useState(false);
+	const handleCancel = () => {
+		setPopupOpen(false);
+		// Additional logic for cancel action
+	};
 	return (
 		<div className="w-32">
-			<Popup />
+			<UserDataPopup />
+
+			<button onClick={() => setPopupOpen(true)}>
+				Open dialog to editing company data
+			</button>
+			{isPopupOpen && (
+				<EditCompanyDataPopup
+					show={true}
+					onCancel={handleCancel}
+					onClose={handleCancel}
+				/>
+			)}
 		</div>
 	);
 };
